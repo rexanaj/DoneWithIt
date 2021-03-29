3c879c36f620f746d66dde4c5615969080741091
rexanaj <66309802+rexanaj@users.noreply.github.com>
2021-03-29 14:30:59 +1100

    Views, texts, images, touchables, buttons and alerts

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  TouchableHighlight, 
+  Image, 
+  SafeAreaView, 
+  Button,
+  Alert,
+} from 'react-native';
 
 export default function App() {
+
+  const handlePress = () => console.log("Text pressed!");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        An app to walk through all the basic features of React Native. 
+      </Text>
+      <TouchableHighlight onPress={() => console.log("Image pressed")} >
+        <Image 
+          source={{ 
+            width: 200,
+            height:300,
+            uri: "https://picsum.photos/200/300",
+          }} />
+      </TouchableHighlight>
+      <Button 
+        title="Click me for an alert" 
+        onPress={() => Alert.alert("This is an alert", "Press an option!", [
+          { text: "Yes", onPress: () => console.log("Yes") }, 
+          { text: "No", onPress: () => console.log("No") },
+        ])} 
+      />
       <StatusBar style="auto" />
-    </View>
+      <Button 
+        title="Click me for a prompt" 
+        onPress={() => Alert.prompt("This is a prompt", "Enter something below!", text => console.log(text))}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1,                  //Fills whole screen 
+    backgroundColor: "#fff",  
     alignItems: 'center',
     justifyContent: 'center',
   },
